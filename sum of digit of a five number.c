818dd37304415f45cf5f46100540499122cae037
Apurva gupta <91661911+appugupta@users.noreply.github.com>
2022-04-17 22:38:29 +0530

    Add files via upload

---
+++ sum of digit of a five number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n, last_num , sum = 0, temp;
+    scanf("%d", &n);
+    //Complete the code to calculate the sum of the five digits on n.
+    temp = n;
+    
+    while(temp > 0)
+    {
+        last_num = temp %10;
+        sum = sum + last_num;
+        temp = temp/10;
+        
+    }
+    printf("%d",sum);
+    
+    return 0;
+}
