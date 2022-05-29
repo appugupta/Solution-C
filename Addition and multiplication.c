7a4f7d6fb24acda6c157ce8207b29e9a69d22518
Apurva gupta <91661911+appugupta@users.noreply.github.com>
2022-05-29 23:40:36 +0530

    Update Addition and multiplication.c

--- Addition and multiplication.c
+++ Addition and multiplication.c
@@ -1,17 +1,4 @@
-/******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include<stdio.h>
 #include<stdlib.h>
