6b17a5f4c8ef83df46cfdffdc4b70aa28c5925be
Apurva gupta <91661911+appugupta@users.noreply.github.com>
2022-05-30 12:06:16 +0530

    Add files via upload

---
+++ Write a Java program to describe usage of throws clause.c
@@ -0,0 +1,22 @@
+package java_programs;
+
+import java.io.*;
+class ThrowExample { 
+  void myMethod(int num)throws IOException, ClassNotFoundException{ 
+     if(num==1)
+        throw new IOException("IOException Occurred");
+     else
+        throw new ClassNotFoundException("ClassNotFoundException");
+  } 
+} 
+
+public class Example1{ 
+  public static void main(String args[]){ 
+   try{ 
+     ThrowExample obj=new ThrowExample(); 
+     obj.myMethod(1); 
+   }catch(Exception ex){
+     System.out.println(ex);
+    } 
+  }
+}
